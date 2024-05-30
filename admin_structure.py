56ad4b5d8efcb4e2d41f5cccde90d9fe7c997cf8
FBI1337 <84200204+FBI1337@users.noreply.github.com>
2024-05-30 14:59:29 +0300

    Сделал завершение чата

--- admin_structure.py
+++ admin_structure.py
@@ -6,6 +6,7 @@ from config import admin_password
 
 
 admin_chat_sessions = {}
+active_chats = {}
 
 def admin(bot):
     
@@ -17,7 +18,7 @@ def admin(bot):
         
     @bot.message_handler(commands=['end_chat'])
     def handle_end_chat_command(message):
-        handle_end_chat(message)
+        end_chat(message.chat.id)
     
     @bot.message_handler(func=lambda message: message.text == 'Больница')
     def handle_hospital(message):
@@ -32,8 +33,9 @@ def admin(bot):
     def handle_devil(message):
         markup = types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True)
         send_message_button = types.KeyboardButton('Отправить сообщение пользователю')
+        end_chat_button = types.KeyboardButton('Завершить чат')
         back_button = types.KeyboardButton('Назад')
-        markup.add(send_message_button, back_button)
+        markup.add(send_message_button, back_button,  end_chat_button)
         bot.send_message(message.chat.id, "Вы в разделе 😈. Пожалуйста, выберите действие:", reply_markup=markup)
         
     @bot.message_handler(func=lambda message: message.text == 'Отправить сообщение пользователю')
@@ -46,6 +48,8 @@ def admin(bot):
         if message.chat.id in admin_chat_sessions.values():
             admin_id = next(key for key, value in admin_chat_sessions.items() if value == message.chat.id)
             bot.send_message(chat_id=admin_id, text=f"Пользователь {message.chat.id} отвечает: {message.text}")
+        elif message.chat.id in active_chats:
+            bot.send_message(message.chat.id, "Ваше сообщение получено.")
         else:
             bot.send_message(message.chat.id, "Ваше сообщение получено.")
     
@@ -86,30 +90,51 @@ def admin(bot):
     @bot.message_handler(func=lambda message: message.text == 'Назад')
     def handle_back(message):
         send_admin_menu(bot, message)
+
+
+    @bot.message_handler(func=lambda message: message.text == "Завершить чат")            
+    def handle_end_chat(message):
+        end_chat(message.chat.id)
+            
             
     def process_user_id_step(message):
         try:
             user_id = int(message.text)
             msg = bot.send_message(message.chat.id, "Введите сообщение:")
             admin_chat_sessions[message.chat.id] = user_id
+            active_chats[user_id] = True
             bot.register_next_step_handler(msg, process_message_step)
         except ValueError:
             bot.send_message(message.chat.id, "ID пользователя должно быть числом.")
 
     def process_message_step(message):
-        admin_id = message.chat.id
-        user_id = admin_chat_sessions.get(admin_id)
-        if user_id:
-            text = message.text
-            if send_message_to_user(bot, user_id, text):
-                bot.send_message(message.chat.id, "Сообщение отправлено. Вы можете продолжать переписку.")
-                # Register next step to continue chatting
-                bot.register_next_step_handler(message, process_message_step)
+        if message.text == 'Завершить чат': 
+            end_chat(message.chat.id)
+        else:      
+            admin_id = message.chat.id
+            user_id = admin_chat_sessions.get(admin_id)
+            if user_id:
+                text = message.text
+                if send_message_to_user(bot, user_id, text):
+                    markup = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
+                    end_chat_button = types.KeyboardButton('Завершить чат')
+                    markup.add(end_chat_button)
+                    bot.send_message(message.chat.id, "Сообщение отправлено. Вы можете продолжать переписку.", reply_markup=markup)
+                    # Register next step to continue chatting
+                    bot.register_next_step_handler(message, process_message_step)
+                else:
+                    bot.send_message(message.chat.id, "Ошибка при отправке сообщения.")
             else:
-                bot.send_message(message.chat.id, "Ошибка при отправке сообщения.")
-        else:
-            bot.send_message(message.chat.id, "Ошибка: не удалось найти пользователя для отправки сообщения.")
+                bot.send_message(message.chat.id, "Ошибка: не удалось найти пользователя для отправки сообщения.")
             
+    def send_message_to_user(bot, user_id, text):
+        try:
+            bot.send_message(chat_id=user_id, text=text)
+            return True
+        except Exception as e: 
+            print(f"Error sending message: {str(e)}")
+            return False
+    
     def process_admin_password_step(message):
         user_id = message.from_user.id
         if message.text == admin_password:
@@ -136,11 +161,14 @@ def admin(bot):
             print(f"Error sending message: {str(e)}")
             return False
         
-    def handle_end_chat(message):
-        admin_id = message.chat.id
+        
+    def end_chat(admin_id):
         if admin_id in admin_chat_sessions:
+            user_id = admin_chat_sessions[admin_id]
             del admin_chat_sessions[admin_id]
-            bot.send_message(message.chat.id, "Чат завершен.")
-            send_admin_menu(bot, message)
+            if user_id in active_chats:
+                del active_chats[user_id]
+            bot.send_message(admin_id, "Чат завершен.")
+            bot.send_message(bot, bot.send_message(admin_id, "Вы завершили чат"))
         else:
-            bot.send_message(message.chat.id, "У вас нет активного чата.")
+            bot.send_message(admin_id, "У вас нет активного чата.")
